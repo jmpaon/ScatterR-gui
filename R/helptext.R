31099d9f2e1b171753dda7bd049ee63d155ab6cd
Juha Panula-Ontto <juha.panulaontto@gmail.com>
2016-01-12 11:31:29 +0200

    help text to a function
    Try to solve the help text issue by moving the help text to an .R file

--- R/helptext.R
+++ R/helptext.R
@@ -1,13 +1,16 @@
+#' Returns the help text for ScatterR-gui function
+
+get_helptext <- function() {return("
 Please consult the Scatter-R documentation 
 for detailed instructions and description of the algorithm.
 
-Start by selecting your data file from "Read CSV datafile".
+Start by selecting your data file from READ CSV DATAFILE.
 Your input data must be in plain CSV format.
 Make the selections concerning the headers and value separator
 of your datafile
-and click "Read file" button.
+and click READ FILE button.
 
-"Select class variable" button opens a dialog where you can 
+SELECT CLASS VARIABLE button opens a dialog where you can 
 select the column that contains the classification variable
 of your datafile. Calculating Scatter values requires 
 that a class variable is indicated in the input data.
@@ -26,16 +29,17 @@ and nominal variables are binarized.
 The GUI, by default, selects numerical variables for scaling
 and factor-like variables for binarization.
 
-"Handling of missing values" option determines how missing values
+HANDLING OF MISSING VALUES option determines how missing values
 are dealt with. The recommended strategy is that
 missing values are replaced with class median (for numerical variables)
 or class mode (for nominal variables) is selected by default.
 
-In the "Other options" section you can select the used distance measure,
+In the OTHER OPTIONS section you can select the used distance measure,
 number of iterations and baseline iterations
 and the calculation procedure.
 Note that the selections affect the calculation time significantly.
 
-After making appropriate selections, press "Calculate" button.
+After making appropriate selections, press CALCULATE button.
 Once the calculation finishes, the results are printed in the R console
 and a dialog where you can plot and save the result is opened.
+")}
